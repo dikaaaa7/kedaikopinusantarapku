bec9cb7764f4572e831d188313520df832ecee08
dikaaaa7 <ranggasyah118@gmail.com>
2024-09-30 12:14:01 +0700

    script.js

--- script.js
+++ script.js
@@ -8,38 +8,29 @@ function openInvitation() {
     }, 600); // Wait for the flap animation to complete
 }
 
-    // Set the date for the grand opening
-    var countDownDate = new Date("Oct 15, 2024 10:00:00").getTime();
+// Countdown Timer
+const countdownElement = document.getElementById('countdown');
+const eventDate = new Date('September 20, 2024 18:00:00').getTime();
 
-    // Update the countdown every 1 second
-    var countdownFunction = setInterval(function() {
+const updateCountdown = () => {
+    const now = new Date().getTime();
+    const distance = eventDate - now;
 
-        // Get today's date and time
-        var now = new Date().getTime();
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        // Calculate the distance between now and the count down date
-        var distance = countDownDate - now;
+    countdownElement.innerHTML = `${days} Day | ${hours} Hour | ${minutes} Minutes `;
 
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    if (distance < 0) {
+        clearInterval(countdownInterval);
+        countdownElement.innerHTML = "Acara Sudah Dimulai!";
+    }
+};
 
-        // Display the result in the respective elements
-        document.getElementById("days").innerHTML = days;
-        document.getElementById("hours").innerHTML = hours;
-        document.getElementById("minutes").innerHTML = minutes;
-        document.getElementById("seconds").innerHTML = seconds;
-
-        // If the countdown is finished, display some text
-        if (distance < 0) {
-            clearInterval(countdownFunction);
-            document.querySelector(".countdown").innerHTML = "Acara Dimulai!";
-        }
-
-    }, 1000);
-
+const countdownInterval = setInterval(updateCountdown, 1000);
+updateCountdown();
 
 
 function createGlitter() {
